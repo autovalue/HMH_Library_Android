6092d411bd9f45efc575cda5c16121cbc89cb2a2
Habeeb Hooshmand <HooshmandFamily@gmail.com>
2012-08-29 19:04:28 -0500

    Chaned Documentation to var.java

--- src/hmh/lang/var.java
+++ src/hmh/lang/var.java
@@ -7,6 +7,14 @@ package hmh.lang;
  * @author Habeeb Hooshmand
  * 
  */
+@Deprecated
+/**
+ * Tiried to Make a "var" equivalent to that of C#, but failed.
+ * 
+ * 
+ * @author Habeeb Hooshmand
+ *
+ */
 public @interface var {
 
 }
